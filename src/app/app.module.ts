24defda27b2af463bf1e4da7f9a9f9b1efbc661e
Frank Fischer <fischertech@Franks-MacBook-Pro.local>
2021-03-23 15:00:09 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import {AdMob} from '@admob-plus/ionic/ngx'
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
@@ -11,7 +13,12 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    AdMob,
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { AdMob } from '@admob-plus/ionic/ngx';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
-
+export class HomePage implements OnInit {
+  banner:any;
+  constructor(public platform: Platform, public admob: AdMob) {}
+  ngOnInit() {
+    this.platform.ready().then(async () => {
+      if(this.platform.is('cordova')) {
+        let adi:string;
+        if(this.platform.is('ios')) adi = 'ca-app-pub-9895747093869252/4786750647';
+        else if(this.platform.is('android')) adi = 'ca-app-pub-9895747093869252/5532037861';
+        if(true) adi = 'ca-app-pub-3940256099942544/6300978111';
+        await this.admob.start();
+        if(!this.banner) {
+          this.banner = new this.admob.BannerAd({
+            adUnitId: adi,
+            position: 'top'
+          });
+        }
+        await this.banner.show();
+      }
+    })
+  }
 }
